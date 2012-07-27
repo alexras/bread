84f5833b7cc99fdc6c6ba91ebfbfef8d94bde254
Alex Rasmussen <alexras@acm.org>
2012-07-26 23:12:58 -0700

    Making bread handle arbitrary bit positions for fields. Adding a few new (as yet poorly tested) types, like Enum (which provides string equivalents to several numeric values).

--- bread/bread.py
+++ bread/bread.py
@@ -6,6 +6,34 @@ import struct, copy
 little_endian = 0
 big_endian = 1
 
+def mask_bits(byte, start_bit, stop_bit):
+    return (((byte << start_bit) & 0xff)
+            >> (7 - stop_bit + start_bit)) & 0xff
+
+def substring_bits(data, start_bit, end_bit):
+    start_byte = start_bit / 8
+    end_byte = end_bit / 8
+
+    shift_amount = start_bit % 8
+
+    byte_list = []
+
+    for current_index in xrange(start_byte, end_byte):
+        current_byte = data[current_index]
+        next_byte = data[current_index + 1]
+
+        first_byte_chunk = mask_bits(current_byte, shift_amount, 7)
+        second_byte_chunk = mask_bits(next_byte, 0, shift_amount - 1)
+
+        shifted_byte = (first_byte_chunk << shift_amount) | second_byte_chunk
+
+        byte_list.append(shifted_byte)
+
+    if (start_byte == end_byte) or (end_bit - start_bit + 1 > 8):
+        byte_list.append(mask_bits(data[end_byte], shift_amount, end_bit % 8))
+
+    return byte_list
+
 class Field(object):
     """
     The base class from which all fields derive.
@@ -64,31 +92,51 @@ class Struct(object):
         return obj
 
     def load(self, data_source):
-        amt_loaded = 0
+        bits_loaded = 0
+
+        if type(data_source) == file:
+            data = map(ord, data_source.read())
+        elif type(data_source) == str:
+            data = map(ord, data_source)
+        elif type(data_source) == bytearray:
+            data = data_source
+        else:
+            raise ValueError("Can't parse data source of type '%s'",
+                             type(data_source))
+
         for field_name in self._bread_field_order:
             field = getattr(self, "_bread_field_" + field_name)
 
-            if type(data_source) == file:
-                data = fp.read(len(field))
-            elif type(data_source) == str:
-                data = data_source[amt_loaded:amt_loaded + len(field)]
+            field_length_bits = len(field)
 
-            field_val = field.load(
-                data, endianness = self._bread_endianness)
-            amt_loaded += len(field)
+            field_bytes = substring_bits(
+                data, bits_loaded, bits_loaded + field_length_bits - 1)
+
+            try:
+                field_val = field.load(
+                    field_bytes, endianness = self._bread_endianness)
+                bits_loaded += field_length_bits
+            except Exception, e:
+                if not e.args:
+                    e.args = ['']
+
+                e.args = (("Parsing field '%s' failed: " % (field_name))
+                          + e.args[0],) + e.args[1:]
+
+                raise e
 
             setattr(self, field_name, field_val)
 
 # Mapping from (length, signed) pairs to struct symbols
 STRUCT_CONVERSION_SYMBOLS = {
-    (1, True) : 'b',
-    (1, False) : 'B',
-    (2, True) : 'h',
-    (2, False) : 'H',
-    (4, True) : 'i',
-    (4, False) : 'I',
-    (8, True) : 'q',
-    (8, False) : 'Q'
+    (8, True) : 'b',
+    (8, False) : 'B',
+    (16, True) : 'h',
+    (16, False) : 'H',
+    (32, True) : 'i',
+    (32, False) : 'I',
+    (64, True) : 'q',
+    (64, False) : 'Q'
     }
 
 class Integer(Field):
@@ -102,8 +150,7 @@ class Integer(Field):
                 conversion = ">"
 
         conversion += self.struct_conversion
-
-        return struct.unpack(conversion, data)[0]
+        return struct.unpack(conversion, ''.join(map(chr, data)))[0]
 
     def __init__(self, length, signed):
         self.struct_conversion = STRUCT_CONVERSION_SYMBOLS[(length, signed)]
@@ -113,39 +160,104 @@ class Integer(Field):
 
 class Int8(Integer):
     def __init__(self):
-        super(Int8, self).__init__(1, True)
+        super(Int8, self).__init__(8, True)
 
 class UInt8(Integer):
     def __init__(self):
-        super(UInt8, self).__init__(1, False)
+        super(UInt8, self).__init__(8, False)
 
 class Int16(Integer):
     def __init__(self):
-        super(Int16, self).__init__(2, True)
+        super(Int16, self).__init__(16, True)
 
 class UInt16(Integer):
     def __init__(self):
-        super(UInt16, self).__init__(2, False)
+        super(UInt16, self).__init__(16, False)
 
 class Int32(Integer):
     def __init__(self):
-        super(Int32, self).__init__(4, True)
+        super(Int32, self).__init__(32, True)
 
 class UInt32(Integer):
     def __init__(self):
-        super(UInt32, self).__init__(4, False)
+        super(UInt32, self).__init__(32, False)
 
 class Int64(Integer):
     def __init__(self):
-        super(Int64, self).__init__(8, True)
+        super(Int64, self).__init__(64, True)
 
 class UInt64(Integer):
     def __init__(self):
-        super(UInt64, self).__init__(8, False)
+        super(UInt64, self).__init__(64, False)
 
 class String(Field):
     def __init__(self, length):
         super(String, self).__init__(length)
 
-    def load(self, data):
-        return struct.unpack("%ds", self.length)
+    def load(self, data, **kwargs):
+        return struct.unpack("%ds" % self.length, data)
+
+class Bool(Field):
+    def __init__(self):
+        super(Bool, self).__init__(1)
+
+    def load(self, data, **kwargs):
+        data = data[0]
+
+        if data == 1:
+            return True
+        elif data == 0:
+            return False
+        else:
+            raise ValueError("Invalid boolean value %d" % (data))
+
+class Padding(Field):
+    def __init__(self, length):
+        super(Padding, self).__init__(length)
+
+    def load(self, data, **kwargs):
+        return None
+
+class Enum(Field):
+    def __init__(self, length, enum_values):
+        self.length = length
+        self.enum_values = enum_values
+
+        if type(enum_values.keys()[0]) is tuple:
+            self.key_ranges = True
+        elif type(enum_values.keys()[0]) is int:
+            self.key_ranges = False
+        else:
+            raise ValueError("Enum keys must be ints or tuples")
+
+        if self.length == 1:
+            self.substruct = Bit()
+        elif self.length == 2:
+            self.substruct = SemiNibble()
+        elif self.length == 4:
+            self.substruct = Nibble()
+        elif self.length == 8:
+            self.substruct = UInt8()
+        elif self.length == 16:
+            self.substruct = UInt16()
+        elif self.length == 32:
+            self.substruct = UInt32()
+        elif self.length == 64:
+            self.substruct = UInt64()
+        else:
+            raise ValueError("bread doesn't currently support enums with keys "
+                             "larger than 64 bits")
+
+    def load(self, data, **kwargs):
+        enum_key = self.substruct.load(data, **kwargs)
+
+        if self.key_ranges:
+            for key_range, val in self.enum_values.items():
+                if enum_key in xrange(*key_range):
+                    return val
+            raise ValueError("%d is not a valid enum value" % (enum_key))
+        else:
+            if enum_key in enum_values:
+                return enum_values[enum_key]
+            else:
+                raise ValueError("%d is not a valid enum value" % (enum_key))
