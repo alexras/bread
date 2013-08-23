c4fe76a3c76c38dbf9b5edebe05fc4ad5913b550
Alex Rasmussen <alexras@acm.org>
2013-08-22 23:17:45 -0700

    Checking in setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(name='bread',
+      version='1.0',
+      description='Binary format parsing made easier',
+      url='https://github.com/alexras/bread',
+      author='Alex Rasmussen',
+      author_email='alexras@acm.org',
+      classifiers = [
+          "Development Status :: 4 - Beta",
+          "Environment :: Other Environment",
+          "Intended Audience :: Developers",
+          "License :: OSI Approved :: MIT License",
+          "Natural Language :: English",
+          "Operating System :: OS Independent",
+          "Programming Language :: Python",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Libraries :: Python Modules"],
+      license='MIT',
+      packages=['bread'],
+      requires=['bitstring'])
