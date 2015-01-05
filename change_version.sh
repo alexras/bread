0d0da749d07288aa5fa0bdc7f06dc40795ff4c9e
Alex Rasmussen <alexras@trifacta.com>
2015-01-04 22:04:00 -0800

    Add version updating script.

---
+++ change_version.sh
@@ -0,0 +1,22 @@
+version=$1
+git_tag="v${version}"
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <version number>"
+    exit 2
+fi
+
+# Set version and build to version
+sed -i .bak -e "s/version = '.*'/version = '${version}'/g" docs/source/conf.py
+sed -i .bak -e "s/release = '.*'/release = '${version}'/g" docs/source/conf.py
+
+# Replace version in __init__.py
+sed -i .bak -e "s/__version__ = '.*'/__version__ = '${version}'/g" bread/__init__.py
+sed -i .bak -e "s/version='.*'/version='${version}'/g" setup.py
+
+# Remove backup files
+find . -name "*.bak" -exec rm {} \;
+
+# Tag revision as this version in git
+git commit -a -m "Bumping version to ${git_tag}"
+git tag ${git_tag}
