b1dac0d2a04630ec47b3490269a48eafcbb46d58
Alex Rasmussen <alexras@acm.org>
2015-04-18 23:24:32 -0700

    Bumping version to v2.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bread',
-      version='2.1.0',
+      version='2.1.1',
       description='Binary format parsing made easier',
       url='https://github.com/alexras/bread',
       author='Alex Rasmussen',
